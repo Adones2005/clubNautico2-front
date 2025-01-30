fe4272c145a59ed51727303c971d7ceb3dfb49b6
Adones2005 <dadogar372@g.educaand.es>
2025-01-30 21:05:49 +0100

    avanzo con el tema de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
+import { ForbiddenComponent } from './features/forbidden/forbidden.component';
+import { Error404Component } from './features/error404/error404.component';
+import { ViajesComponent } from './features/viajes/viajes.component';
+import { authGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // Ruta inicial
+    component: HomeComponent,
+  },
+  {
+    path: 'login', // Página de inicio de sesión
+    component: LoginComponent,
+  },
+  {
+    path: 'viajes', // Página protegida
+    component: ViajesComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'forbidden',
+    component: ForbiddenComponent
+  },
+  {
+    path: '**', // Ruta común para 404
+    component: Error404Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { ForbiddenComponent } from './features/forbidden/forbidden.component';
+import { Error404Component } from './features/error404/error404.component';
+import { LoginComponent } from './features/login/login.component';
+import { ViajesComponent } from './features/viajes/viajes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ForbiddenComponent,
+    Error404Component,
+    LoginComponent,
+    ViajesComponent
   ],
   imports: [
     BrowserModule,
